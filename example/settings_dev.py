7d89226afc94a1510a7662f18661bdf07db57c7b
Xabier Larrakoetxea <slok69@gmail.com>
2012-12-28 17:46:31 +0100

    Configure the example app

---
+++ example/settings_dev.py
@@ -0,0 +1,8 @@
+from settings import *
+
+# Email settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'user@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
